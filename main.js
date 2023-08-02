21484ff957f47c865f5f3838843b43c4a56891eb
ezequielmagi <ezequielmaggi@gmail.com>
2023-08-02 11:06:55 -0300

    Clase 1

---
+++ main.js
@@ -0,0 +1,27 @@
+alert('Bienvenidos a JS')
+//Este es un comentario.
+
+//Mostrar un mensaje en consola.
+console.log('Hola mundo')
+console.log('Esto es js')
+
+//tipos de datos: number
+let myNumber = 7
+
+let myNewNumber = myNumber + 1
+
+const myConst = 10
+
+let myNumberNotInit
+
+console.log(myNumber + 5)
+
+//Tipo de dato: string
+
+let myString = 'hola mundo'
+
+let myNewString = "Esto es un texto con 'comillas' se supone"
+
+let myStringNotInit
+
+let Name
